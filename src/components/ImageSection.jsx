84d15b7086e0d6d7331f6d1f7860585580546ee0
PabloM <paanmuav@gmail.com>
2024-06-12 18:59:49 -0500

    Fuentes, algunos estilos

--- src/components/ImageSection.jsx
+++ src/components/ImageSection.jsx
@@ -1,32 +1,44 @@
-import React from 'react';
+import React from "react";
 
 const HeroBanner = ({ title, content, image, bgImage, bgColor, flipColumn }) => {
-  const columnDirection = flipColumn ? 'md:flex-row-reverse' : 'md:flex-row';
-  const imgStyles = 'object-cover object-center w-full h-full md:h-auto';
+  const columnDirection = flipColumn ? "md:flex-row-reverse" : "md:flex-row";
+  const imgStyles =
+    "object-cover object-center w-full h-full md:h-auto rounded-lg shadow-2xl";
 
-  // Determinar el color de fondo y la opacidad
-  const fondoEstilos = bgImage ? {
-    backgroundImage: `url(${bgImage})`,
-    backgroundColor: '',
-    opacity: 1, // Cambiado a 0.6 para la opacidad
-  } : {
-    backgroundImage: `none`, // Cambiado para limpiar el background image cuando no hay bgImage
-    backgroundColor: `#${bgColor}`,
-    opacity: 1,
-  };
+  // Determinar el color o imagen de fondo
+  const bgStyles = bgImage
+    ? {
+        backgroundImage: `url(${bgImage})`,
+        backgroundColor: "",
+        opacity: 1,
+      }
+    : {
+        backgroundImage: `none`,
+        backgroundColor: `#${bgColor}`,
+        opacity: 1,
+      };
+
+  // Obtener el nombre del archivo de la imagen para usarlo como alt text
+  const altText = image ? getImageAltText(image) : "Hero Image";
+
+  function getImageAltText(imageUrl) {
+    const startIndex = imageUrl.lastIndexOf("/") + 12;
+    const endIndex = imageUrl.lastIndexOf(".");
+    return imageUrl.substring(startIndex, endIndex);
+  }
 
   return (
-    <div className="py-20" style={{ ...fondoEstilos }}>
+    <div className="py-20" style={{ ...bgStyles }}>
       <div className={`container mx-auto md:flex-row flex flex-col items-center justify-center gap-8 px-6 ${columnDirection}`}>
         <div className="flex flex-col w-full md:w-6/12 mb-8 md:mb-0 md:mr-6">
           <div className="max-w-lg">
-            <h2 className="text-4xl font-bold text-gray-800 mb-4">{title}</h2>
-            <p className="text-lg text-gray-700 mb-8">{content}</p>
+            <h2 className="text-4xl font-bold mb-4">{title}</h2>
+            <p className="text-lg mb-8">{content}</p>
           </div>
         </div>
 
         <div className="flex items-center justify-center w-full md:w-6/12">
-          <img src={image} alt="Hero" className={imgStyles} />
+          <img src={image} alt={altText} className={imgStyles} />
         </div>
       </div>
     </div>
